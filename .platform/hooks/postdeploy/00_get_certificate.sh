1ae71de7dbaafcb20e6579ea65e891be5fd8cd65
Gunnjake <Gunnjakeim@gmail.com>
2025-12-10 15:21:55 -0700

    commit

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d huus.is404.net --nginx --agree-tos --email kenfife128@hotmail.coms
+sudo certbot -n -d huus.is404.net --nginx --agree-tos --email kenfife128@hotmail.com
